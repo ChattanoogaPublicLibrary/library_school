94844990fff5f7492ab5c03b6e8deb2d2ffed370
Sean Brewer <seabre986@gmail.com>
2014-05-31 17:37:06 -0400

    Add links to courses and proposals on frontend.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def proposal_course_pth(obj)
+    if obj.is_a?(Proposal)
+      proposal_path(obj)
+    elsif obj.is_a?(Course)
+      course_path(obj)
+    end
+  end
 end

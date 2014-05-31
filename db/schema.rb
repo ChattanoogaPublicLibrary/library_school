10e8a9b757a1e58dab37f2b9dde4eb067a841f0a
Sean Brewer <seabre986@gmail.com>
2014-05-31 13:57:15 -0400

    Add votes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531173834) do
+ActiveRecord::Schema.define(version: 20140531175044) do
 
   create_table "courses", force: true do |t|
     t.boolean  "status"
@@ -39,5 +39,15 @@ ActiveRecord::Schema.define(version: 20140531173834) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["course_id"], name: "index_votes_on_course_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
